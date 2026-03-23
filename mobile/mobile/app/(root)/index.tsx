e61edd1b5047dd2181343595c2a7800e06c6d954
Lawal <lrafiou491@gmail.com>
2026-03-24 00:24:49 +0100

    update file

---
+++ mobile/mobile/app/(root)/index.tsx
@@ -0,0 +1,42 @@
+import { SignOutButton } from '@/components/SignOutButton'
+// import { Text } from '@/components/themed-text'
+// import { View } from '@/components/themed-view'
+import { Show, useSession, useUser } from '@clerk/expo'
+import { Link } from 'expo-router'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Page() {
+  const { user } = useUser()
+
+  // If your user isn't appearing as signed in,
+  // it's possible they have session tasks to complete.
+  // Learn more: https://clerk.com/docs/guides/configure/session-tasks
+  const { session } = useSession()
+  console.log(session?.currentTask)
+
+  return (
+    <View style={styles.container}>
+      <Text type="title">Welcome!</Text>
+      <Show when="signed-out">
+        <Link href="/(auth)/sign-in">
+          <Text>Sign in</Text>
+        </Link>
+        <Link href="/(auth)/sign-up">
+          <Text>Sign up</Text>
+        </Link>
+      </Show>
+      <Show when="signed-in">
+        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+        <SignOutButton />
+      </Show>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    gap: 16,
+  },
+})
---
+++ mobile/mobile/components/SafeScreen.tsx
@@ -0,0 +1,16 @@
+import { COLORS } from '@/constants/colors';
+import { View, Text } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+
+const SafeScreen = ({ children }: any) => {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View style={{ paddingTop: insets.top, flex: 1, backgroundColor: COLORS.background }}>
+      {children}
+    </View>
+  )
+}
+
+export default SafeScreen
